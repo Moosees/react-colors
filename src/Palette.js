1d6dda3e5f150270f4527a5e2f5385677a3e9dfa
Moosees <linus.almgren@gmail.com>
2019-05-07 17:15:47 +0200

    Add Palette component

---
+++ src/Palette.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Palette = () => (
+  <div className="Palette">
+    {/* NAVBAR */}
+    <div className="Palette-colors">{/* Color box components */}</div>
+    {/* FOOTER */}
+  </div>
+);
+
+export default Palette;
