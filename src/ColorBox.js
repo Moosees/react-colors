50a60a915c3be3514439eef48c0c5c655e2d0cb5
Moosees <linus.almgren@gmail.com>
2019-05-08 19:09:44 +0200

    Add react-copy-to-clipboard

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 
 const ColorBox = ({ background, name }) => (
-  <div className="ColorBox" style={{ backgroundColor: background }}>
-    <div className="ColorBox-copy-container">
-      <div className="ColorBox-content">
-        <span>{name}</span>
+  <CopyToClipboard text={background}>
+    <div className="ColorBox" style={{ backgroundColor: background }}>
+      <div className="ColorBox-copy-container">
+        <div className="ColorBox-content">
+          <span>{name}</span>
+        </div>
+        <button className="ColorBox-btn ColorBox-btn-copy">Copy</button>
       </div>
-      <button className="ColorBox-btn ColorBox-btn-copy">Copy</button>
+      <button className="ColorBox-btn ColorBox-btn-more">More</button>
     </div>
-    <button className="ColorBox-btn ColorBox-btn-more">More</button>
-  </div>
+  </CopyToClipboard>
 );
 
 export default ColorBox;
