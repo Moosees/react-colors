dd288d4410c204ddf0ed6b456491242aa0b4af05
Moosees <linus.almgren@gmail.com>
2019-05-15 20:58:22 +0200

    Add more shades and style to SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -3,9 +3,31 @@ import Navbar from './Navbar';
 import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFooter';
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/styles';
 import { gatherShades } from './singleColorHelpers';
 
-const SingleColorPalette = ({ palette, paletteId, colorId }) => {
+const styles = {
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh'
+  },
+  colors: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(5, 1fr)',
+    height: '90%',
+    lineHeight: '0'
+  },
+  backBox: {
+    backgroundColor: '#333',
+    gridColumn: '5 / 6',
+    gridRow: '4 / span 3',
+    cursor: 'pointer',
+    position: 'relative'
+  }
+};
+
+const SingleColorPalette = ({ classes, palette, paletteId, colorId }) => {
   const [format, setFormat] = useState('hex');
   const shades = useMemo(() => gatherShades(palette, colorId), [
     palette,
@@ -20,20 +42,18 @@ const SingleColorPalette = ({ palette, paletteId, colorId }) => {
   };
 
   return (
-    <div className="SingleColorPalette Palette">
+    <div className={classes.root}>
       <Navbar changeFormat={changeFormat} />
-      <div className="Palette-colors">
+      <div className={classes.colors}>
         {colorBoxes}
-        <Link to={`/palette/${paletteId}`}>
-          <div className="ColorBox" style={{ backgroundColor: '#000' }}>
-            <button className="ColorBox-btn ColorBox-btn-copy ColorBox-btn-back">
-              Go Back
-            </button>
-          </div>
+        <Link to={`/palette/${paletteId}`} className={classes.backBox}>
+          <button className="ColorBox-btn ColorBox-btn-copy ColorBox-btn-back">
+            Go Back
+          </button>
         </Link>
       </div>
       <PaletteFooter paletteName={palette.paletteName} emoji={palette.emoji} />
     </div>
   );
 };
-export default SingleColorPalette;
+export default withStyles(styles)(SingleColorPalette);
--- src/singleColorHelpers.js
+++ src/singleColorHelpers.js
@@ -1,12 +1,66 @@
+import chroma from 'chroma-js';
+
+const saturateShade = shade => {
+  const saturatedShade = {
+    name: `${shade.name}S`,
+    id: `${shade.id}S`,
+    shortId: `${shade.shortId}S`,
+    hex: chroma(shade.hex)
+      .saturate(1.1)
+      .hex(),
+    rgb: chroma(shade.hex)
+      .saturate(1.1)
+      .css(),
+    rgba: chroma(shade.hex)
+      .saturate(1.1)
+      .css()
+      .replace('rgb', 'rgba')
+      .replace(')', ',1.0)')
+  };
+  return saturatedShade;
+};
+
+const deSaturateShade = shade => {
+  const deSaturatedShade = {
+    name: `${shade.name}DS`,
+    id: `${shade.id}DS`,
+    shortId: `${shade.shortId}DS`,
+    hex: chroma(shade.hex)
+      .desaturate(1.1)
+      .hex(),
+    rgb: chroma(shade.hex)
+      .desaturate(1.1)
+      .css(),
+    rgba: chroma(shade.hex)
+      .desaturate(1.1)
+      .css()
+      .replace('rgb', 'rgba')
+      .replace(')', ',1.0)')
+  };
+  return deSaturatedShade;
+};
+
 const gatherShades = (palette, colorId) => {
-  let shades = [];
   const allColors = palette.colors;
+  let shades = Array(29);
+  let dsCounter = 0;
+  let baseCounter = 5;
+  let sCounter = 10;
 
   for (let key in allColors) {
     if (key % 100 === 0) {
-      shades = shades.concat(
-        allColors[key].filter(color => color.shortId === colorId)
+      const baseShade = allColors[key].filter(
+        color => color.shortId === colorId
       );
+      if(dsCounter === 5) {
+        dsCounter = 15;
+        baseCounter = 20;
+        sCounter = 25;
+      }
+      shades[dsCounter++] = deSaturateShade(baseShade[0]);
+      shades[baseCounter++] = baseShade[0];
+      shades[sCounter++] = saturateShade(baseShade[0]);
+      console.log(shades);
     }
   }
   return shades;
