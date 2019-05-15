b147acc08ddee0f950f5c16b34d93b3b075d32c5
Moosees <linus.almgren@gmail.com>
2019-05-15 21:52:22 +0200

    Change ColorBox text color to adjust to background luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 class ColorBox extends Component {
   state = { copied: false };
+
+  handleCopy = () => {
+    this.setState({ copied: true }, () => {
+      setTimeout(() => {
+        this.setState({ copied: false });
+      }, 1500);
+    });
+  };
 
   render() {
     const { name, bgColor, moreUrl } = this.props;
@@ -12,16 +21,11 @@ class ColorBox extends Component {
       ? ' ColorBox-copy-overlay--show'
       : '';
     const colorboxMsgShow = this.state.copied ? ' ColorBox-copy-msg--show' : '';
-    const handleCopy = () => {
-      this.setState({ copied: true }, () => {
-        setTimeout(() => {
-          this.setState({ copied: false });
-        }, 1500);
-      });
-    };
+    const isDarkColor = chroma(bgColor).luminance() <= 0.08;
+    const isLightColor = chroma(bgColor).luminance() >= 0.55;
 
     return (
-      <CopyToClipboard text={bgColor} onCopy={handleCopy}>
+      <CopyToClipboard text={bgColor} onCopy={this.handleCopy}>
         <div className="ColorBox" style={{ backgroundColor: bgColor }}>
           <div
             className={`ColorBox-copy-overlay${colorboxCopyShow}`}
@@ -33,12 +37,28 @@ class ColorBox extends Component {
           </div>
           <div className="ColorBox-content">
             <div className="ColorBox-text">
-              <span>{name}</span>
+              <span
+                className={isDarkColor ? 'ColorBox-text--light' : undefined}
+              >
+                {name}
+              </span>
             </div>
-            <button className="ColorBox-btn ColorBox-btn-copy">Copy</button>
+            <button className="ColorBox-btn ColorBox-btn-copy">
+              <span
+                className={isLightColor ? 'ColorBox-text--dark' : undefined}
+              >
+                Copy
+              </span>
+            </button>
             {moreUrl && (
               <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                <button className="ColorBox-btn ColorBox-btn-more">More</button>
+                <button className="ColorBox-btn ColorBox-btn-more">
+                  <span
+                    className={isLightColor ? 'ColorBox-text--dark' : undefined}
+                  >
+                    More
+                  </span>
+                </button>
               </Link>
             )}
           </div>
