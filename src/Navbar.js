91b934db4993da55fc350cdbcaab53361262d99d
Moosees <linus.almgren@gmail.com>
2019-05-16 00:46:36 +0200

    Refactor Navbar styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import { withStyles } from '@material-ui/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import './Navbar.css';
 
 const theme = createMuiTheme({
   typography: {
@@ -19,7 +19,61 @@ const theme = createMuiTheme({
   }
 });
 
-const Navbar = ({ level, changeLevel, changeFormat }) => {
+const styles = {
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: '6vh'
+  },
+  logo: {
+    color: '#000',
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%',
+    marginRight: '1.5rem',
+    padding: '0 1rem',
+    fontSize: '1.4rem',
+    textDecoration: 'none',
+    backgroundColor: '#ebebeb'
+  },
+  sliderContainer: {
+    display: 'flex',
+    '& span': { fontSize: '1.2rem' }
+  },
+  slider: {
+    display: 'inline-block',
+    width: '34rem',
+    margin: '0 1.5rem',
+    '& .rc-slider-track': {
+      backgroundColor: 'transparent'
+    },
+    '& .rc-slider-rail': {
+      height: 8,
+      marginTop: '-1px',
+      background: 'linear-gradient(90deg, #eee, #333)'
+    },
+    '& .rc-slider-handle': {
+      height: 15,
+      width: 15,
+      marginLeft: '-3px',
+      outline: 'none',
+      boxShadow: 'none',
+      backgroundColor: '#00bcd4',
+      border: '2px solid #00bcd4',
+      transition: 'transform 0.1s ease-in-out',
+      '&:hover, &:active': {
+        transform: 'scale(1.2)'
+      }
+    }
+  },
+  formatSelect: {
+    marginLeft: 'auto',
+    marginRight: '1rem'
+  }
+};
+
+const Navbar = ({ classes, level, changeLevel, changeFormat }) => {
   const [format, setFormat] = useState('hex');
   const [open, setOpen] = useState(false);
 
@@ -33,14 +87,14 @@ const Navbar = ({ level, changeLevel, changeFormat }) => {
   };
 
   return (
-    <nav className="Navbar">
-      <div className="Navbar-logo">
-        <Link to="/">color playground</Link>
-      </div>
+    <nav className={classes.root}>
+      <Link to="/" className={classes.logo}>
+        color playground
+      </Link>
       {level && (
-        <div className="Navbar-slider-container">
+        <div className={classes.sliderContainer}>
           <span>Level: {level}</span>
-          <div className="Navbar-slider">
+          <div className={classes.slider}>
             <Slider
               defaultValue={level}
               min={50}
@@ -51,7 +105,7 @@ const Navbar = ({ level, changeLevel, changeFormat }) => {
           </div>
         </div>
       )}
-      <div className="Navbar-format-select">
+      <div className={classes.formatSelect}>
         <MuiThemeProvider theme={theme}>
           <Select value={format} onChange={handleFormatChange}>
             <MenuItem value="hex">HEX - #eeeeee</MenuItem>
@@ -84,4 +138,4 @@ const Navbar = ({ level, changeLevel, changeFormat }) => {
   );
 };
 
-export default Navbar;
+export default withStyles(styles)(Navbar);
