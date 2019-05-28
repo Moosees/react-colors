5a76fea9333c992123c6dcb96e7f56a9fdddae5a
Moosees <linus.almgren@gmail.com>
2019-05-28 09:01:20 +0200

    Updates

---
+++ precache-manifest.d3b28774bced9dc29198f5f8d117672f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691af19bb2da0ad75dd1dc137175597b",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "dbedfa438f1008a58a6c",
+    "url": "/react-colors/static/css/2.e337d257.chunk.css"
+  },
+  {
+    "revision": "c915ea5a7af0bec3245c",
+    "url": "/react-colors/static/css/main.2a6519a3.chunk.css"
+  },
+  {
+    "revision": "dbedfa438f1008a58a6c",
+    "url": "/react-colors/static/js/2.d4d8df09.chunk.js"
+  },
+  {
+    "revision": "c915ea5a7af0bec3245c",
+    "url": "/react-colors/static/js/main.399c799c.chunk.js"
+  },
+  {
+    "revision": "b824bab327c7f9fd3f68",
+    "url": "/react-colors/static/js/runtime~main.665c9a02.js"
+  }
+]);
