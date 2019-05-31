9e2fd2116d2eda1ea859a4e179e42e579fc93720
Moosees <linus.almgren@gmail.com>
2019-05-31 22:19:46 +0200

    Refactor App.js to functional component and clean up code

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState, useMemo, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import Page from './Page';
+import { Route, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { FADE_TIME } from '../constants';
+import { generatePalette } from '../helpers/paletteHelpers';
+import seedColors from '../helpers/seedColors';
 import NewPaletteForm from './NewPaletteForm';
+import Page from './Page';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
-import seedColors from '../helpers/seedColors';
-import { generatePalette } from '../helpers/paletteHelpers';
-import { FADE_TIME } from '../constants';
 
 const fades = {
   '@global': {
@@ -30,139 +30,113 @@ const fades = {
   }
 };
 
-class App extends Component {
-  state = {
-    palettes: this.getLocalPalettes(),
-    format: 'hex'
+const App = () => {
+  const localPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+  const [format, setFormat] = useState('hex');
+  const [palettes, setPalettes] = useState(localPalettes || seedColors);
+
+  useEffect(() => {
+    window.localStorage.setItem('palettes', JSON.stringify(palettes));
+  }, [palettes]);
+
+  const paletteNames = useMemo(() => palettes.map(item => item.paletteName), [
+    palettes
+  ]);
+
+  const changeFormat = format => {
+    setFormat(format);
   };
 
-  getLocalPalettes() {
-    const localPalettes = JSON.parse(window.localStorage.getItem('palettes'));
-    return localPalettes ? localPalettes : seedColors;
-  }
+  const findPalette = id => palettes.find(palette => palette.id === id);
 
-  syncLocalStorage() {
-    window.localStorage.setItem(
-      'palettes',
-      JSON.stringify(this.state.palettes)
-    );
-  }
-
-  changeFormat = format => {
-    this.setState({ format });
+  const savePalette = newPalette => {
+    setPalettes([...palettes, newPalette]);
   };
 
-  findPalette = id => {
-    return this.state.palettes.find(palette => palette.id === id);
+  const deletePalette = paletteId => {
+    setPalettes(palettes.filter(palette => palette.id !== paletteId));
   };
 
-  savePalette = newPalette => {
-    this.setState(
-      { palettes: [...this.state.palettes, newPalette] },
-      this.syncLocalStorage
-    );
+  const resetPaletteList = () => {
+    setPalettes(seedColors);
   };
 
-  deletePalette = paletteId => {
-    this.setState(
-      {
-        palettes: this.state.palettes.filter(
-          palette => palette.id !== paletteId
-        )
-      },
-      this.syncLocalStorage
-    );
-  };
-
-  resetPaletteList = () => {
-    this.setState({ palettes: [] }, () => {
-      setTimeout(() => {
-        this.setState({ palettes: seedColors }, this.syncLocalStorage);
-      }, FADE_TIME);
-    });
-  };
-
-  render() {
-    const { palettes, format } = this.state;
-    const paletteNames = palettes.map(item => item.paletteName);
-
-    return (
-      <Route
-        render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              classNames="fade"
-              timeout={FADE_TIME}
-              key={location.key}
-            >
-              <Switch location={location}>
-                <Route
-                  exact
-                  path="/palette/new"
-                  render={routeProps => (
-                    <Page>
-                      <NewPaletteForm
-                        savePalette={this.savePalette}
-                        paletteNames={paletteNames}
-                        starterPalette={seedColors[0].colors}
-                        {...routeProps}
-                      />
-                    </Page>
-                  )}
-                />
-                <Route
-                  exact
-                  path="/"
-                  render={routeProps => (
-                    <Page>
-                      <PaletteList
-                        paletteList={palettes}
-                        deletePalette={this.deletePalette}
-                        resetPaletteList={this.resetPaletteList}
-                        {...routeProps}
-                      />
-                    </Page>
-                  )}
-                />
-                <Route
-                  exact
-                  path="/palette/:id"
-                  render={routeProps => (
-                    <Page>
-                      <Palette
-                        palette={generatePalette(
-                          this.findPalette(routeProps.match.params.id)
-                        )}
-                        format={format}
-                        changeFormat={this.changeFormat}
-                      />
-                    </Page>
-                  )}
-                />
-                <Route
-                  exact
-                  path="/palette/:paletteId/:colorId"
-                  render={routeProps => (
-                    <Page>
-                      <SingleColorPalette
-                        palette={generatePalette(
-                          this.findPalette(routeProps.match.params.paletteId)
-                        )}
-                        paletteId={routeProps.match.params.paletteId}
-                        colorId={routeProps.match.params.colorId}
-                        format={format}
-                        changeFormat={this.changeFormat}
-                      />
-                    </Page>
-                  )}
-                />
-              </Switch>
-            </CSSTransition>
-          </TransitionGroup>
-        )}
-      />
-    );
-  }
-}
+  return (
+    <Route
+      render={({ location }) => (
+        <TransitionGroup>
+          <CSSTransition
+            classNames="fade"
+            timeout={FADE_TIME}
+            key={location.key}
+          >
+            <Switch location={location}>
+              <Route
+                exact
+                path="/palette/new"
+                render={routeProps => (
+                  <Page>
+                    <NewPaletteForm
+                      savePalette={savePalette}
+                      paletteNames={paletteNames}
+                      starterPalette={seedColors[0].colors}
+                      {...routeProps}
+                    />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path="/"
+                render={routeProps => (
+                  <Page>
+                    <PaletteList
+                      paletteList={palettes}
+                      deletePalette={deletePalette}
+                      resetPaletteList={resetPaletteList}
+                      {...routeProps}
+                    />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path="/palette/:id"
+                render={routeProps => (
+                  <Page>
+                    <Palette
+                      palette={generatePalette(
+                        findPalette(routeProps.match.params.id)
+                      )}
+                      format={format}
+                      changeFormat={changeFormat}
+                    />
+                  </Page>
+                )}
+              />
+              <Route
+                exact
+                path="/palette/:paletteId/:colorId"
+                render={routeProps => (
+                  <Page>
+                    <SingleColorPalette
+                      palette={generatePalette(
+                        findPalette(routeProps.match.params.paletteId)
+                      )}
+                      paletteId={routeProps.match.params.paletteId}
+                      colorId={routeProps.match.params.colorId}
+                      format={format}
+                      changeFormat={changeFormat}
+                    />
+                  </Page>
+                )}
+              />
+            </Switch>
+          </CSSTransition>
+        </TransitionGroup>
+      )}
+    />
+  );
+};
 
 export default withStyles(fades)(App);
