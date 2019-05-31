a0d2892a47692f08a40c423f63e3b0fe5b31d9bc
Moosees <linus.almgren@gmail.com>
2019-05-31 23:16:31 +0200

    Refactor ColorBox to functional component

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,60 +1,64 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { useState, useEffect } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import classNames from 'classnames';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Link } from 'react-router-dom';
 import styles from '../styles/ColorBoxStyles';
 
-class ColorBox extends Component {
-  state = { copied: false };
+const ColorBox = ({ classes, name, bgColor, moreUrl }) => {
+  const [copied, setCopied] = useState(false);
 
-  handleCopy = () => {
-    this.setState({ copied: true }, () => {
-      setTimeout(() => {
-        this.setState({ copied: false });
+  useEffect(() => {
+    if (copied) {
+      let copyTimer = setTimeout(() => {
+        setCopied(false);
       }, 1500);
-    });
+      return () => clearTimeout(copyTimer);
+    }
+  }, [copied]);
+
+  const handleCopy = () => {
+    setCopied(true);
   };
 
-  render() {
-    const { classes, name, bgColor, moreUrl } = this.props;
-    const { copied } = this.state;
+  const handleClick = e => {
+    e.stopPropagation();
+  };
 
-    return (
-      <CopyToClipboard text={bgColor} onCopy={this.handleCopy}>
-        <div className={classes.root}>
-          <div style={{ visibility: !copied && 'hidden' }}>
-            <div
-              className={classNames(classes.copyOverlay, {
-                [classes.copyOverlayShow]: copied
-              })}
-            />
-            <div
-              className={classNames(classes.copyMsg, {
-                [classes.copyMsgShow]: copied
-              })}
-            >
-              <h1>Copied!</h1>
-              <p className={classes.btnText}>{bgColor}</p>
-            </div>
-          </div>
-          <div>
-            <div className={classes.mainText}>{name}</div>
-            <button className={`${classes.btn} ${classes.btnCopy}`}>
-              <span className={classes.btnText}>Copy</span>
-            </button>
-            {moreUrl && (
-              <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                <button className={`${classes.btn} ${classes.btnMore}`}>
-                  <span className={classes.btnText}>More</span>
-                </button>
-              </Link>
-            )}
+  return (
+    <CopyToClipboard text={bgColor} onCopy={handleCopy}>
+      <div className={classes.root}>
+        <div style={{ visibility: !copied && 'hidden' }}>
+          <div
+            className={classNames(classes.copyOverlay, {
+              [classes.copyOverlayShow]: copied
+            })}
+          />
+          <div
+            className={classNames(classes.copyMsg, {
+              [classes.copyMsgShow]: copied
+            })}
+          >
+            <h1>Copied!</h1>
+            <p className={classes.btnText}>{bgColor}</p>
           </div>
         </div>
-      </CopyToClipboard>
-    );
-  }
-}
+        <div>
+          <div className={classes.mainText}>{name}</div>
+          <button className={classNames(classes.btn, classes.btnCopy)}>
+            <span className={classes.btnText}>Copy</span>
+          </button>
+          {moreUrl && (
+            <Link to={moreUrl} onClick={handleClick}>
+              <button className={classNames(classes.btn, classes.btnMore)}>
+                <span className={classes.btnText}>More</span>
+              </button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </CopyToClipboard>
+  );
+};
 
 export default withStyles(styles)(ColorBox);
